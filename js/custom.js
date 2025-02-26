b09f01ac208e7c39db7c41f2406f342c9f0ccf42
pedro <pedro@jadebar.co.uk>
2025-02-26 22:00:47 +0000

    Small changes

--- js/custom.js
+++ js/custom.js
@@ -7,18 +7,6 @@
   $('.navbar-collapse a').on('click',function(){
     $(".navbar-collapse").collapse('hide');
   });
-
-  $(function() {
-    $('.hero-slides').vegas({
-        slides: [
-            { src: 'images/slides/sincere-laugh-showing-picture-smartphone-casual-meeting-with-best-friends-restaurant-terrace.jpg' },
-            { src: 'images/happy-waitress-giving-coffee-customers-while-serving-them-coffee-shop.jpg' },
-            { src: 'images/young-female-barista-wear-face-mask-serving-take-away-hot-coffee-paper-cup-consumer-cafe.jpg' }
-        ],
-        timer: false,
-        animation: 'kenburns',
-    });
-  });
   
   // CUSTOM LINK
   $('.smoothscroll').click(function(){
