9e7b76266e45188bd75b00cc1a3f7b58b088185e
pedro <pedro@jadebar.co.uk>
2025-05-05 22:12:59 +0100

    small cleanups

--- js/click-scroll.js
+++ js/click-scroll.js
@@ -1,22 +1,22 @@
 var sectionArray = [1, 2, 3, 4];
 
 $.each(sectionArray, function(index, value){
-          
+
      $(document).scroll(function(){
          var offsetSection = $('#' + 'section_' + value).offset().top - 154;
          var docScroll = $(document).scrollTop();
          var docScroll1 = docScroll + 1;
-         
-        
+
+
          if ( docScroll1 >= offsetSection ){
              $('.navbar-nav .nav-link').removeClass('active');
              $('.navbar-nav .nav-link:link').addClass('inactive');  
              $('.navbar-nav .nav-item .nav-link').eq(index).addClass('active');
              $('.navbar-nav .nav-item .nav-link').eq(index).removeClass('inactive');
          }
-         
+
      });
-    
+
     $('.click-scroll').eq(index).click(function(e){
         var offsetClick = $('#' + 'section_' + value).offset().top - 154;
         e.preventDefault();
